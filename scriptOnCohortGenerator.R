8f4a190bef4788df48573c4288e5bdeb61e295c6
Gowtham Rao <rao@ohdsi.org>
2023-03-17 05:25:07 -0400

    Update scriptOnCohortGenerator.R

--- scriptOnCohortGenerator.R
+++ scriptOnCohortGenerator.R
@@ -46,33 +46,154 @@ featureCohortIds <- PhenotypeLibrary::getPhenotypeLog() |>
 cohortDefinitionSet <-
   PhenotypeLibrary::getPlCohortDefinitionSet(cohortIds = c(targetCohortIds) |> unique())
 
+addCohortDefinitionSubset <-
+  function(cohortDefinitionSet,
+           subsetOperators,
+           definitionId,
+           targetCohortIds,
+           overwriteExists = TRUE) {
+    cohortSubsetDefintion = CohortGenerator::createCohortSubsetDefinition(name = '',
+                                                                          definitionId = definitionId,
+                                                                          subsetOperators = subsetOperators)
+    
+    modifiedCohortDefinitionSet <-
+      CohortGenerator::addCohortSubsetDefinition(
+        cohortDefinitionSet = cohortDefinitionSet,
+        cohortSubsetDefintion = cohortSubsetDefintion,
+        targetCohortIds = targetCohortIds,
+        overwriteExisting = TRUE
+      )
+    
+    return(modifiedCohortDefinitionSet)
+  }
+
+firstEvent <- CohortGenerator::createLimitSubset(
+  name = "(First ever)",
+  priorTime = 0,
+  followUpTime = 0,
+  limitTo = "firstEver"
+)
 
 earliestOccurrenceWith365 <- CohortGenerator::createLimitSubset(
-  name = "Earliest event with 365 days prior observation time",
+  name = "(Earliest event with 365 days prior observation time)",
   priorTime = 365,
   followUpTime = 0,
   limitTo = "earliestRemaining"
 )
 
-
 lastEver <- CohortGenerator::createLimitSubset(
-  name = "Last ever event",
+  name = "(Last ever event)",
   priorTime = 0,
   followUpTime = 0,
   limitTo = "lastEver"
 )
 
-cohortDefinitionSet <- cohortDefinitionSet |>
-  CohortGenerator::addCohortSubsetDefinition(
-    cohortSubsetDefintion = CohortGenerator::createCohortSubsetDefinition(
-      name = '',
-      definitionId = 2,
-      subsetOperators = list(lastEver)
-    ),
-    targetCohortIds = targetCohortIds,
-    overwriteExisting = TRUE
+maleOnly <-
+  CohortGenerator::createDemographicSubset(name = "(Male only)",
+                                           gender = 8532)
+
+femaleOnly <-
+  CohortGenerator::createDemographicSubset(name = "(Female only)",
+                                           gender = 8507)
+
+pediatricOnly <-
+  CohortGenerator::createDemographicSubset(name = "(Pediatric only)",
+                                           ageMax = 18)
+
+adultOnly <-
+  CohortGenerator::createDemographicSubset(name = "(Adult 18 to 65 only)",
+                                           ageMin = 18,
+                                           ageMax = 65)
+
+adultMaleOnly <-
+  CohortGenerator::createDemographicSubset(
+    name = "(Adult 18 to 65 Male only)",
+    gender = 8532,
+    ageMin = 18,
+    ageMax = 65
   )
 
+adultFemaleOnly <-
+  CohortGenerator::createDemographicSubset(
+    name = "(Adult 18 to 65 Female only)",
+    gender = 8507,
+    ageMin = 18,
+    ageMax = 65
+  )
+
+cohortDefinitionSet <-
+  addCohortDefinitionSubset(
+    cohortDefinitionSet = cohortDefinitionSet,
+    subsetOperators = list(firstEvent),
+    definitionId = 1,
+    targetCohortIds = targetCohortIds
+  )
+
+cohortDefinitionSet <-
+  addCohortDefinitionSubset(
+    cohortDefinitionSet = cohortDefinitionSet,
+    subsetOperators = list(earliestOccurrenceWith365),
+    definitionId = 2,
+    targetCohortIds = targetCohortIds
+  )
+
+cohortDefinitionSet <-
+  addCohortDefinitionSubset(
+    cohortDefinitionSet = cohortDefinitionSet,
+    subsetOperators = list(lastEver),
+    definitionId = 3,
+    targetCohortIds = targetCohortIds
+  )
+
+cohortDefinitionSet <-
+  addCohortDefinitionSubset(
+    cohortDefinitionSet = cohortDefinitionSet,
+    subsetOperators = list(maleOnly),
+    definitionId = 4,
+    targetCohortIds = targetCohortIds
+  )
+
+cohortDefinitionSet <-
+  addCohortDefinitionSubset(
+    cohortDefinitionSet = cohortDefinitionSet,
+    subsetOperators = list(femaleOnly),
+    definitionId = 5,
+    targetCohortIds = targetCohortIds
+  )
+
+cohortDefinitionSet <-
+  addCohortDefinitionSubset(
+    cohortDefinitionSet = cohortDefinitionSet,
+    subsetOperators = list(pediatricOnly),
+    definitionId = 6,
+    targetCohortIds = targetCohortIds
+  )
+
+cohortDefinitionSet <-
+  addCohortDefinitionSubset(
+    cohortDefinitionSet = cohortDefinitionSet,
+    subsetOperators = list(adultOnly),
+    definitionId = 7,
+    targetCohortIds = targetCohortIds
+  )
+
+cohortDefinitionSet <-
+  addCohortDefinitionSubset(
+    cohortDefinitionSet = cohortDefinitionSet,
+    subsetOperators = list(adultMaleOnly),
+    definitionId = 8,
+    targetCohortIds = targetCohortIds
+  )
+
+cohortDefinitionSet <-
+  addCohortDefinitionSubset(
+    cohortDefinitionSet = cohortDefinitionSet,
+    subsetOperators = list(adultFemaleOnly),
+    definitionId = 9,
+    targetCohortIds = targetCohortIds
+  )
+
+# remove persons in second from 1st - this gives
 
 cohortTableNames = CohortGenerator::getCohortTableNames(cohortTable =
                                                           paste0("c", studyName, "_", cdmSource$sourceId))
@@ -109,19 +230,27 @@ CohortGenerator::generateCohortSet(
 )
 
 
-FeatureExtractionSettingsCohortDiagnostics <- CohortDiagnostics::getDefaultCovariateSettings()
-FeatureExtractionSettingsCohortBasedCovariateSettings <- FeatureExtraction::createCohortBasedTemporalCovariateSettings(
-  analysisId = 150,
-  covariateCohortDatabaseSchema = cohortDatabaseSchema,
-  covariateCohortTable = paste0(stringr::str_squish("pl_"),
-                                stringr::str_squish(cdmSource$sourceKey)),
-  covariateCohorts = 
-    PhenotypeLibrary::getPlCohortDefinitionSet(cohortIds = featureCohortIds |> unique()),
-  valueType = "binary",
-  temporalStartDays = FeatureExtractionSettingsCohortDiagnostics$temporalStartDays,
-  temporalEndDays = FeatureExtractionSettingsCohortDiagnostics$temporalEndDays
-)
-FeatureExtractionCovariateSettings <- list(FeatureExtractionSettingsCohortDiagnostics, FeatureExtractionSettingsCohortBasedCovariateSettings)
+FeatureExtractionSettingsCohortDiagnostics <-
+  CohortDiagnostics::getDefaultCovariateSettings()
+FeatureExtractionSettingsCohortBasedCovariateSettings <-
+  FeatureExtraction::createCohortBasedTemporalCovariateSettings(
+    analysisId = 150,
+    covariateCohortDatabaseSchema = cohortDatabaseSchema,
+    covariateCohortTable = paste0(
+      stringr::str_squish("pl_"),
+      stringr::str_squish(cdmSource$sourceKey)
+    ),
+    covariateCohorts =
+      PhenotypeLibrary::getPlCohortDefinitionSet(cohortIds = featureCohortIds |> unique()),
+    valueType = "binary",
+    temporalStartDays = FeatureExtractionSettingsCohortDiagnostics$temporalStartDays,
+    temporalEndDays = FeatureExtractionSettingsCohortDiagnostics$temporalEndDays
+  )
+FeatureExtractionCovariateSettings <-
+  list(
+    FeatureExtractionSettingsCohortDiagnostics,
+    FeatureExtractionSettingsCohortBasedCovariateSettings
+  )
 
 ## Execute Cohort Diagnostics on remote ----
 CohortDiagnostics::executeDiagnostics(
@@ -136,12 +265,15 @@ CohortDiagnostics::executeDiagnostics(
   connectionDetails = connectionDetails,
   cohortTableNames = cohortTableNames,
   vocabularyDatabaseSchema = vocabularyDatabaseSchema,
-  temporalCovariateSettings = FeatureExtractionCovariateSettings,
+  temporalCovariateSettings = FeatureExtractionSettingsCohortDiagnostics,
+  #FeatureExtractionCovariateSettings,
   incremental = TRUE
 )
 
 
 # package results ----
+wd <- getwd()
+setwd(outputFolder)
 CohortDiagnostics::createMergedResultsFile(dataFolder = outputFolder, overwrite = TRUE)
 # Launch diagnostics explorer shiny app ----
 CohortDiagnostics::launchDiagnosticsExplorer()
